6b5d030d2891e6c75e5e328d6daaae613c5ceb24
Vivek Kamboj <43985107+Vivek-Kamboj@users.noreply.github.com>
2021-10-02 12:38:48 +0530

    #2 Improved Globe function.

--- index.cpp
+++ index.cpp
@@ -3,11 +3,15 @@ using namespace std;
  
 int main ()
 {
+    cout<<"Enter Number of Globes";
+    int n;
+    cin>>n;
+    while(n--){
     cout<<"Enter Globe description";
     string input;
     cin>>input;
     
     cout<<input<< " is the description you entered\n";
-     
+    }
     return 0;
 }
